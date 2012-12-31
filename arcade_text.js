d3f50d173895b434c77f2391578a2b491bfc05c3
calamari <g.tavonius@gmail.com>
2012-12-31 13:11:29 +0100

    Added lower case characters, added some brackets/braces and optimized the overall font

--- arcade_text.js
+++ arcade_text.js
@@ -8,16 +8,19 @@
 ;(function(win, doc, undef) {
   "use strict";
   var ArcadeFont = {
+    // Problems with this font:
+    // - the Y is not 7 width
+    // - the j somehow feels not quite right
     alphabet: {
       'A': '28,54,99,99,127,99,99',
       'B': '63,99,99,63,99,99,63',
       'C': '62,99,3,3,3,99,62',
       'D': '31,51,99,99,99,51,31',
       'E': '127,3,3,63,3,3,127',
-      'F': '63,3,3,31,3,3,3,0',
+      'F': '63,3,3,31,3,3,3',
       'G': '62,99,3,115,99,99,62',
-      'H': '99,99,99,127,99,99,99,0',
-      'I': '30,12,12,12,12,12,30,0',
+      'H': '99,99,99,127,99,99,99',
+      'I': '30,12,12,12,12,12,30',
       'J': '96,96,96,96,96,99,62',
       'K': '99,51,27,15,27,51,99',
       'L': '3,3,3,3,3,3,127',
@@ -35,6 +38,32 @@
       'X': '99,119,62,28,62,119,99',
       'Y': '51,51,51,30,12,12,12',
       'Z': '127,112,56,28,14,7,127',
+      'a': '0,0,62,96,126,99,126',
+      'b': '3,3,3,63,99,99,63',
+      'c': '0,0,62,3,3,3,62',
+      'd': '96,96,96,126,99,99,126',
+      'e': '0,0,62,99,127,3,62',
+      'f': '56,12,30,12,12,12,12',
+      'g': '0,0,126,99,99,126,96,62',
+      'h': '3,3,3,63,99,99,99',
+      'i': '12,12,0,14,12,12,30',
+      'j': '24,24,0,28,24,24,24,14',
+      'k': '3,3,99,51,31,51,99',
+      'l': '14,12,12,12,12,12,30',
+      'm': '0,0,55,127,107,107,99',
+      'n': '0,0,31,51,51,51,51',
+      'o': '0,0,62,99,99,99,62',
+      'p': '0,0,63,99,99,63,3,3',
+      'q': '0,0,126,99,99,126,96,96',
+      'r': '0,0,31,51,3,3,3',
+      's': '0,0,62,3,62,96,63',
+      't': '12,12,30,12,12,12,56',
+      'u': '0,0,99,99,99,99,126',
+      'v': '0,0,99,99,54,28,8',
+      'w': '0,0,99,107,107,127,54',
+      'x': '0,0,99,54,28,54,99',
+      'y': '0,0,99,99,99,126,96,62',
+      'z': '0,0,127,56,28,14,127',
       ' ': '0,0,0,0,0,0,0',
       '1': '12,14,12,12,12,12,63',
       '2': '62,99,112,60,6,3,127',
@@ -46,18 +75,24 @@
       '8': '62,99,99,62,99,99,62',
       '9': '62,99,99,126,96,99,62',
       '0': '62,99,99,99,99,99,62',
+      '(': '24,12,6,6,6,12,24',
+      ')': '6,12,24,24,24,12,6',
+      '{': '24,12,12,6,12,12,24',
+      '}': '6,12,12,24,12,12,6',
       "'": '12,12,6,0,0,0,0',
       '"': '102,102,51,0,0,0,0',
       '.': '0,0,0,0,0,6,6',
       '!': '6,6,6,6,0,0,6',
-      ',': '0,0,0,0,12,12,6',
-      ':': '0,6,6,0,6,6,0',
-      ';': '0,12,12,0,12,12,6',
+      ',': '0,0,0,0,0,6,6,3',
+      ':': '0,6,6,0,0,6,6',
+      ';': '0,6,6,0,0,6,6,3',
       '?': '62,99,48,24,12,0,12',
-      '$': '8,62,11,62,104,62,8'
+      '$': '8,62,3,62,96,63,8'
     },
 
+
     // space between letters
+    // That is effectivle no 8 bit no more, because 7 + 2 are 9 ;-)
     gutter: 2,
 
     version: '0.2.0',
@@ -65,12 +100,14 @@
     blueprint: function(text) {
       var blueprint = [],
           letter, letterCode, line, g, i, l, j;
-      text = text.toUpperCase();
       for (i in text) {
         letter = this.alphabet[text[i]];
         if (letter) {
           letterCode = letter.split(',');
-
+          // add the eighth line for rows that haven't defined it
+          if (letterCode.length < 8) {
+            letterCode.push('');
+          }
           for (j = 0, l = letterCode.length; j<l; ++j) {
             line = EightBit.decodeNumber(letterCode[j], 7);
             if (!blueprint[j]) {
@@ -97,12 +134,12 @@
       return codes.join(',');
     },
 
-    decode: function(code, base) {
+    decode: function(code, fontWidth) {
       var result = [],
           lines = code.split(','),
           i, l;
       for (i = 0, l = lines.length; i<l; ++i) {
-        result.push(this.decodeNumber(lines[i], base));
+        result.push(this.decodeNumber(lines[i], fontWidth));
       }
       return result.join('\n');
     },
@@ -115,10 +152,10 @@
       return x;
     },
 
-    decodeNumber: function(nr, base) {
+    decodeNumber: function(nr, fontWidth) {
       var line = '',
           i, p;
-      for (i = base; i--;) {
+      for (i = fontWidth; i--;) {
         p = Math.pow(2, i);
         if (p <= nr) {
           nr -= p;
@@ -145,7 +182,7 @@
 
     // calculate canvas Size
     options.canvas.width = 6 * text.length * (options.pixelSize + options.gutter);
-    options.canvas.height = 7 * options.pixelSize;
+    options.canvas.height = 8 * options.pixelSize;
 
     // draws the text onto own canvas
     function draw() {
